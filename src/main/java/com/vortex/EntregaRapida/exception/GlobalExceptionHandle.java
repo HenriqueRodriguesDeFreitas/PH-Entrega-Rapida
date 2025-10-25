284a72ea37e9a41d3b30d08b9b3276746d20d38c
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-10-25 15:13:12 -0300

    fix: adicionando tratamento de erro para conflito de entidades

--- src/main/java/com/vortex/EntregaRapida/exception/GlobalExceptionHandle.java
+++ src/main/java/com/vortex/EntregaRapida/exception/GlobalExceptionHandle.java
@@ -1,7 +1,33 @@
 package com.vortex.EntregaRapida.exception;
 
+import com.vortex.EntregaRapida.dto.response.ErroResponseDto;
+import com.vortex.EntregaRapida.exception.custom.ConflitoDeEntidadeException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandle {
+
+    ErroResponseDto response;
+
+    @ExceptionHandler(ConflitoDeEntidadeException.class)
+    public ResponseEntity<ErroResponseDto> ConflitoDeEntidadeHandle(
+            ConflitoDeEntidadeException ex) {
+        response = toResponse(
+                ex, HttpStatus.CONFLICT, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    private static ErroResponseDto toResponse(Exception e,
+                                              HttpStatus status,
+                                              String mensagem) {
+        return new ErroResponseDto(LocalDateTime.now(),
+                status.value(),
+                e.getMessage(),
+                mensagem);
+    }
 }
